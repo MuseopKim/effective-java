90af629719300f104b9d68e3ce07af2ca0473b70
ragdoll <museopkim0214@gmail.com>
2022-06-17 23:59:20 +0900

    feat: builder

---
+++ src/main/java/chapter01/item02/builder/NutritionFacts.java
@@ -0,0 +1,67 @@
+package chapter01.item02.builder;
+
+public class NutritionFacts {
+
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static void main(String[] args) {
+        NutritionFacts cocaCola = new Builder(240, 8)
+                .calories(100)
+                .sodium(35)
+                .carbohydrate(27).build();
+    }
+
+    public static class Builder {
+
+        // 필수 매개변수
+        private final int servingSize;
+        private final int servings;
+        private int calories;
+        private int fat;
+        private int sodium;
+        private int carbohydrate;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int val) {
+            calories = val;
+            return this;
+        }
+
+        public Builder fat(int val) {
+            fat = val;
+            return this;
+        }
+
+        public Builder sodium(int val) {
+            sodium = val;
+            return this;
+        }
+
+        public Builder carbohydrate(int val) {
+            carbohydrate = val;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(Builder builder) {
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbohydrate = builder.carbohydrate;
+    }
+}
